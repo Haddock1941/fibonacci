191a73e3bb6fb9ddae63a4627ac669ce1eafcb1f
Haddock <averagetryhardbear@gmail.com>
2022-09-28 16:17:02 +0200

    Add memoization

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
+use std::collections::HashMap;
+
 /// fib
 /// ---
 /// fib(0) = 0
 /// fib(1) = 1
 /// fib(n) = fib(n-1) + fib(n-2) for all n >= 0
-pub fn fib(n: usize) -> usize {
-    match n {
-        0 => 0,
-        1 => 1,
-        n => fib(n - 1) + fib(n - 2),
+///
+/// * `cache` - A HashMap mapping (n => fib(n)) for caching results
+pub fn fib(n: usize, cache: &mut HashMap<usize, usize>) -> usize {
+    // tbh in real life I would use https://crates.io/crates/memoize
+    // looks well maintained and there's rarely a reason to reinvent the wheel
+    // but since this is a tech task and the point is probably to prove I've
+    // understood what memoization even is, here you go:
+    match cache.get(&n) {
+        Some(cached_res) => *cached_res,
+        None => {
+            match n {
+                0 => 0,
+                1 => 1,
+                n => {
+                    let res = fib(n - 1, cache) + fib(n - 2, cache);
+                    cache.insert(n, res);
+                    res
+                },
+            }
+        }
     }
+
 }
 
 
@@ -16,13 +34,15 @@ pub fn fib(n: usize) -> usize {
 mod tests {
     use crate::fib;
 
+    use std::collections::HashMap;
+
     #[test]
     fn test_fib_7() {
-        assert_eq!(fib(7), 13);
+        assert_eq!(fib(7, &mut HashMap::new()), 13);
     }
 
     #[test]
     fn test_fib_31() {
-        assert_eq!(fib(31), 1346269);
+        assert_eq!(fib(31, &mut HashMap::new()), 1346269);
     }
 }
